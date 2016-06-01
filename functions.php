42773c3e89159828604722fb24e5fe04b4d77901
smarkdurbin <s@markdurb.in>
2016-06-01 15:18:52 +0000

    ran theme check widget, added wordpress classes to wordpress.css

--- functions.php
+++ functions.php
@@ -9,8 +9,6 @@ require_once('inc/helpers/wp_bootstrap_navwalker.php');
 add_theme_support('automatic-feed-links');
 
 add_theme_support( "title-tag" );
-add_theme_support( "custom-header", $args );
-add_theme_support( "custom-background", $args );
 add_editor_style();
 
 // declare thumbnail sizes
